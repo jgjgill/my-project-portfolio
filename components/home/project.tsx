8c5aeba3ee7947bed8a513f9cd91496b507cd6c1
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-02-20 20:54:47 +0900

    Style - Home페이지 스타일링

--- components/home/project.tsx
+++ components/home/project.tsx
@@ -4,10 +4,14 @@ interface ProjectProps {}
 
 const Project = ({}: ProjectProps) => {
   return (
-    <div className="bg-slate-200 w-full py-4 px-2 text-center space-y-8">
-      <ProjectContainer title='프로젝트1' date='2022-02-09' address='jgjgill' git='git' />
-      <ProjectContainer title='프로젝트2' date='2022-02-10' address='jgjgill' git='git' />
-      <ProjectContainer title='프로젝트3' date='2022-02-11' address='jgjgill' git='git' />
+    <div className="bg-slate-400 w-full py-4 px-2 text-center space-y-8">
+      <ProjectContainer
+        title="프로젝트1"
+        date="2022-02-09"
+        git="https://github.com/jgjgill/my_project_portfolio"
+      />
+      <ProjectContainer title="프로젝트2" date="2022-02-10" git="git" />
+      <ProjectContainer title="프로젝트3" date="2022-02-11" git="git" />
     </div>
   );
 };
