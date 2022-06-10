be723c3ec29e442886324ef591a0ccb1966ef4d3
jgjgill <79239852+jgjgill@users.noreply.github.com>
2022-06-10 11:15:22 +0900

    Refactoring - myProjectContainer 리팩토링

--- components/home/myProjectContainer/projectDescription.tsx
+++ components/home/myProjectContainer/projectDescription.tsx
@@ -1,32 +1,15 @@
-import Link from 'next/link'
+import ProjectDescriptionItem from './projectDescriptionItem'
+import { IProjectDescription } from './myBlog'
 
 interface ProjectDescriptionProps {
-  descriptions: { title: string; content: string; link: boolean }[]
+  descriptions: IProjectDescription[]
 }
 
 const ProjectDescription = ({ descriptions }: ProjectDescriptionProps) => {
   return (
     <div className='space-y-4 text-left px-2'>
-      {descriptions.map((description, i) => (
-        <div key={i} className='space-y-2'>
-          {description.link ? (
-            <>
-              <h3 className='text-slate-400 text-2xl font-semibold'>Address</h3>
-              <p>
-                <Link href={`https://${description.content}`} passHref>
-                  <a target='_blank' rel='noopener noreferrer' className='text-slate-50 text-lg font-semibold'>
-                    {description.content}
-                  </a>
-                </Link>
-              </p>
-            </>
-          ) : (
-            <>
-              <h3 className='text-slate-400 text-2xl font-semibold'>{description.title}</h3>
-              <p className='text-slate-50 text-lg font-semibold'>{description.content}</p>
-            </>
-          )}
-        </div>
+      {descriptions.map((description) => (
+        <ProjectDescriptionItem key={description.title} description={description} />
       ))}
     </div>
   )
